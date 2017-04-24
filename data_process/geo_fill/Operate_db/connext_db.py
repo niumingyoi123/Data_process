89e5738d2e6a56fdd3a07d8685017966489db948
Ethan <niumingyoi123@msn.cn>
2017-04-24 16:20:17 +0800

    connect Database

---
+++ data_process/geo_fill/Operate_db/connext_db.py
@@ -0,0 +1,16 @@
+import pymysql
+
+db = pymysql.connect("localhost","root","","user_trajectory")
+cursor = db.cursor()
+cursor.execute("DROP TABLE IF EXISTS USER_TRAJ")
+sql = """CREATE TABLE USER_TRAJ (
+        ID INT auto_increment primary key,
+        TIMESTAMP DATETIME,
+        DEVICEID CHAR(30) NOT NULL,
+        LONGITUDE DECIMAL(9,6),
+        LATITUDE  DECIMAL(9,6)
+)"""
+
+cursor.execute(sql)
+
+db.close()
